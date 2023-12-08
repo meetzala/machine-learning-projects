424ba045776b9a291968575a87cf433ea73f406c
Meet Zala <73380817+meetzala@users.noreply.github.com>
2023-12-08 15:55:13 +0530

    Delete machine-learning-projects directory

--- machine-learning-projects/README.md
+++
@@ -1,1 +0,0 @@
-# Data science end to end project
--- machine-learning-projects/python_boilerplate/python_boilerplate/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for Python Boilerplate."""
-
-__author__ = """Aman Singh"""
-__email__ = 'amansi7359@gmail.com'
-__version__ = '0.1.0'
